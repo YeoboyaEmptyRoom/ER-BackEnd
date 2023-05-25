bf1bee0c4105c80e6c6963466700469d7f0ed4e5
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2023-05-26 01:16:05 +0900

    :bug: Manager isn't available; 'auth.User' has been swapped for 'myapp.CustomUser' - views.py를 통해서 작성한 이메일을 통해서 받은 인증번호를 입력하면 위와 같은 오류가 발생한다 - 오류의 발생의 위치는 settings.py,models.py

--- empty/myapp/models.py
+++ empty/myapp/models.py
@@ -1,3 +1,7 @@
+# models.py
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    id = models.AutoField(primary_key=True)
