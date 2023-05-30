3ce076038150a9fc41b96b5de88a85fcc1959d9f
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2023-05-30 13:30:29 +0900

    :fire: 파일 삭제

--- empty/myapp/models.py
+++
@@ -1,9 +0,0 @@
-# models.py
-from django.contrib.auth.models import AbstractBaseUser
-from django.db import models
-
-
-class CustomUser(AbstractBaseUser):
-    email = models.EmailField(unique=True)
-    # 필요한 다른 필드들
-    USERNAME_FIELD = "email"
--- empty/myapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UserRegistrationView, VerificationView
-
-urlpatterns = [
-    path("register/", UserRegistrationView.as_view(), name="user-registration"),
-    path("verify/", VerificationView.as_view(), name="verification"),
-]
