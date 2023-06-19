b77e7b49a437889d90ae83fe972fcab050f459d3
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2023-06-19 09:06:59 +0900

    signup & sign

--- empty/myapp/views.py
+++ empty/myapp/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
+from rest_framework import generics, status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+User = get_user_model()
+
+
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            {"message": "회원가입이 성공적으로 완료되었습니다."}, status=201, headers=headers
+        )
+
+
+class UserLoginView(generics.GenericAPIView):
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key}, status=status.HTTP_200_OK)
+        else:
+            return Response(
+                {"message": "유효하지 않은 로그인 정보입니다."}, status=status.HTTP_401_UNAUTHORIZED
+            )
